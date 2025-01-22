b3c64bfc85e156c4293d2b8f9df5566a8919717a
claddenhero <glebbka98@mail.ru>
2025-01-22 22:07:01 +0300

    1

---
+++ stuff/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'stuff'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('place', views.place, name='place'),
+
+]
